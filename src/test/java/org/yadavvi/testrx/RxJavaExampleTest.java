3ce279a4520dd777ca9a3955bf152de9b0da7c66
Vishal Yadav <yadavvi91@gmail.com>
2017-02-17 11:47:29 +0530

    The test for the testUsingComputationScheduler() fails. This is because we are testing on another thread whereas the data is being subscribed on another thread (here Schedulers.computation()).

--- src/test/java/org/yadavvi/testrx/RxJavaExampleTest.java
+++ src/test/java/org/yadavvi/testrx/RxJavaExampleTest.java
@@ -5,6 +5,7 @@ import io.reactivex.annotations.NonNull;
 import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Consumer;
 import io.reactivex.observers.TestObserver;
+import io.reactivex.schedulers.Schedulers;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -85,6 +86,27 @@ public class RxJavaExampleTest {
         // then
         observer.assertError(exception);
         observer.assertNotComplete();
+    }
+
+    @Test
+    public void testUsingComputationScheduler() {
+        // given
+        TestObserver<String> observer = new TestObserver<String>();
+        Observable<String> observable = Observable.fromIterable(WORDS)
+                .zipWith(Observable.range(1, Integer.MAX_VALUE), new BiFunction<String, Integer, String>() {
+                    public String apply(@NonNull String word, @NonNull Integer index) throws Exception {
+                        return String.format("%2d. %s", index, word);
+                    }
+                });
+
+        // when
+        observable.subscribeOn(Schedulers.computation())
+                .subscribe(observer);
+
+        // then
+        observer.assertComplete();
+        observer.assertNoErrors();
+        assertThat(observer.values(), hasItem(" 4. fox"));
     }
 
 }
