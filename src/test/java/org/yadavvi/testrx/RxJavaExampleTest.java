3b15c7f89684ff7f6f64b25b9c8f26550cda848f
Vishal Yadav <yadavvi91@gmail.com>
2017-02-17 11:45:20 +0530

    Using a TestObserver instead of a accumulator(here a List). The TestObserver has methods that can give us the values() and other such things.

--- src/test/java/org/yadavvi/testrx/RxJavaExampleTest.java
+++ src/test/java/org/yadavvi/testrx/RxJavaExampleTest.java
@@ -4,6 +4,7 @@ import io.reactivex.Observable;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Consumer;
+import io.reactivex.observers.TestObserver;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -12,7 +13,7 @@ import java.util.List;
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 import static org.yadavvi.testrx.RxJavaExample.WORDS;
 
 /**
@@ -43,4 +44,47 @@ public class RxJavaExampleTest {
         assertThat(result, hasSize(9));
         assertThat(result, hasItem(" 4. fox"));
     }
+
+    @Test
+    public void testUsingTestObserver() {
+        // given
+        TestObserver<String> observer = new TestObserver<String>();
+        Observable<String> observable = Observable.fromIterable(WORDS)
+                .zipWith(Observable.range(1, Integer.MAX_VALUE), new BiFunction<String, Integer, String>() {
+                    public String apply(@NonNull String word, @NonNull Integer index) throws Exception {
+                        return String.format("%2d. %s", index, word);
+                    }
+                });
+
+        // when
+        observable.subscribe(observer);
+
+        // then
+        observer.assertComplete();
+        observer.assertNoErrors();
+        observer.assertValueCount(9);
+        assertThat(observer.values(), hasItem(" 4. fox"));
+    }
+
+    @Test
+    public void testFailure() {
+        // given
+        TestObserver<String> observer = new TestObserver<String>();
+        Exception exception = new RuntimeException("aslkdfj");
+        Observable<String> observable = Observable.fromIterable(WORDS)
+                .zipWith(Observable.range(1, Integer.MAX_VALUE), new BiFunction<String, Integer, String>() {
+                    public String apply(@NonNull String word, @NonNull Integer index) throws Exception {
+                        return String.format("%2d. %s", index, word);
+                    }
+                })
+                .concatWith(Observable.<String>error(exception));
+
+        // when
+        observable.subscribe(observer);
+
+        // then
+        observer.assertError(exception);
+        observer.assertNotComplete();
+    }
+
 }
